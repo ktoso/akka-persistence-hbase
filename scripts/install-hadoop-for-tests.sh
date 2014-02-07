6f3045b248ea7cacbe6395b67fc96135fe2632a5
Konrad Malawski <konrad.malawski@project13.pl>
2014-02-07 01:32:18 +0000

    argh typo

--- scripts/install-hadoop-for-tests.sh
+++ scripts/install-hadoop-for-tests.sh
@@ -4,7 +4,7 @@ BUILD_DIR=`pwd`
 
 wget --no-check-certificate http://raw.github.com/fs111/grrrr/master/grrr -O /tmp/grrr && chmod +x /tmp/grrr
 /tmp/grrr /hadoop/common/hadoop-1.2.1/hadoop-1.2.1.tar.gz -O /tmp/hadoop.tar.gz --read-timeout=5 --tries=0
-sodo mkdir -p /opt
+sudo mkdir -p /opt
 sudo tar xf /tmp/hadoop.tar.gz -C /opt
 sudo chown -R `whoami`.`whoami` /opt
 
