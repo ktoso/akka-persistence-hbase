5e6e030401d00327f91eab89707112092cc8f3fe
Konrad Malawski <konrad.malawski@project13.pl>
2014-02-07 01:30:05 +0000

    install to opt

--- scripts/install-hbase-for-tests.sh
+++ scripts/install-hbase-for-tests.sh
@@ -6,4 +6,6 @@ hbase_home="/opt/hbase-$hbase_version"
 
 
 wget "http://archive.apache.org/dist/hbase/hbase-$hbase_version/hbase-$hbase_version.tar.gz" -O /tmp/hbase.tar.gz --read-timeout=5 --tries=0
-tar xf /tmp/hbase.tar.gz -C /opt
+sudo mkdir -p /opt
+sudo tar xf /tmp/hbase.tar.gz -C /opt
+sudo chown -R `whoami`.`whoami` /opt
